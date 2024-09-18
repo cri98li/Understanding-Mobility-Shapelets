ab8fc84410f7372ec48a4db9b8524043425da9eb
cri98li <c.landi7@studenti.unipi.it>
2024-09-18 12:27:33 +0200

    Update sumo_test_transferability.py

--- code/benchmark/sumo_test_transferability.py
+++ code/benchmark/sumo_test_transferability.py
@@ -16,12 +16,12 @@ from run_model_from_transformations import evaluate_clf
 
 def no_fit(geo, dest_dataset_X_test, dest_dataset_y_test):
     y_pred = geo.predict(dest_dataset_X_test)
-    return evaluate_clf(y_pred, dest_dataset_y_test), geo
+    return evaluate_clf(y_pred, dest_dataset_y_test, None), geo
 
 def refit_classifier_new_data(geo, dest_dataset_X_train, dest_dataset_y_train, dest_dataset_X_test, dest_dataset_y_test):
     geo.model_to_fit.fit(geo.transform(dest_dataset_X_train), dest_dataset_y_train)
     y_pred = geo.predict(dest_dataset_X_test)
-    return evaluate_clf(y_pred, dest_dataset_y_test), geo
+    return evaluate_clf(y_pred, dest_dataset_y_test, None), geo
 
 def refit_classifier_all_data(geo, source_dataset_X_train, source_dataset_y_train, source_dataset_X_test, source_dataset_y_test,
                               dest_dataset_X_train, dest_dataset_y_train, dest_dataset_X_test, dest_dataset_y_test):
@@ -30,14 +30,14 @@ def refit_classifier_all_data(geo, source_dataset_X_train, source_dataset_y_trai
 
     X_train_all = np.append(source_transformed, des_transformed, axis=0)
 
-    geo.model_to_fit.fit(X_train_all, np.append(source_dataset_y_train, dest_dataset_y_train), axis=0)
+    geo.model_to_fit.fit(X_train_all, np.append(source_dataset_y_train, dest_dataset_y_train, axis=0))
     y_pred_source = geo.predict(source_dataset_X_test)
     y_pred_dest = geo.predict(dest_dataset_X_test)
     y_pred_all = np.append(y_pred_source, y_pred_dest, axis=0)
 
-    res_source = evaluate_clf(y_pred_source, source_dataset_y_test)
-    res_dest = evaluate_clf(y_pred_dest, dest_dataset_y_test)
-    res_all = evaluate_clf(y_pred_all, np.append(source_dataset_y_test, dest_dataset_y_test))
+    res_source = evaluate_clf(y_pred_source, source_dataset_y_test, None)
+    res_dest = evaluate_clf(y_pred_dest, dest_dataset_y_test, None)
+    res_all = evaluate_clf(y_pred_all, np.append(source_dataset_y_test, dest_dataset_y_test), None)
 
 
     return {f"source_{k}": v for k, v in res_source.items()} \
@@ -60,13 +60,14 @@ def preprocess(df0:pd.DataFrame):
 
 if __name__ == "__main__":
     datasets = {
-        "sumo_all_car_vs_bikes": preprocess(r.sumo_all_car_vs_bikes()),
+        "sumo_cities_car_vs_bikes": preprocess(r.sumo_cities_car_vs_bikes()),
         "sumo_grid_car_vs_bikes": preprocess(r.sumo_grid_car_vs_bikes()),
         "sumo_borgo_car_vs_bikes": preprocess(r.sumo_borgo_car_vs_bikes()),
         "sumo_mixed_car_vs_bikes": preprocess(r.sumo_mixed_car_vs_bikes()),
     }
 
     for model_path in tqdm(glob('results/final_models/*.pkl'), position=0, leave=False):
+        origin_dataset_name = model_path.split("/")[-1].split("|")[0]
         if origin_dataset_name not in datasets:
             continue
 
@@ -82,17 +83,17 @@ if __name__ == "__main__":
 
             if not os.path.exists(f"results/transfered_models/{model_name}|{dataset_name}_no.pkl"):
                 res, new_geo = no_fit(geo, dest_X_test, dest_y_test)
-                pd.DataFrame.from_dict([res]).to_csv(f"results/transfered_models/{model_name}|{dataset_name}_no.pkl", index=False)
+                pd.DataFrame.from_dict([res]).to_csv(f"results/transfered_models/{model_name}|{dataset_name}_no.csv", index=False)
                 pickle.dump(new_geo, open(f"results/transfered_models/{model_name}|{dataset_name}_no.pkl", "wb"))
 
             if not os.path.exists(f"results/transfered_models/{model_name}|{dataset_name}_partial.pkl"):
                 res, new_geo = refit_classifier_new_data(geo, dest_X_train, dest_y_train, dest_X_test, dest_y_test)
-                pd.DataFrame.from_dict([res]).to_csv(f"results/transfered_models/{model_name}|{dataset_name}_partial.pkl", index=False)
+                pd.DataFrame.from_dict([res]).to_csv(f"results/transfered_models/{model_name}|{dataset_name}_partial.csv", index=False)
                 pickle.dump(new_geo, open(f"results/transfered_models/{model_name}|{dataset_name}_partial.pkl", "wb"))
 
             if not os.path.exists(f"results/transfered_models/{model_name}|{dataset_name}_all.pkl"):
-                res, new_geo = refit_classifier_new_data(geo, source_X_train, source_y_train, source_X_test, source_y_test,
+                res, new_geo = refit_classifier_all_data(geo, source_X_train, source_y_train, source_X_test, source_y_test,
                                                          dest_X_train, dest_y_train, dest_X_test, dest_y_test)
-                pd.DataFrame.from_dict([res]).to_csv(f"results/transfered_models/{model_name}|{dataset_name}_all.pkl", index=False)
+                pd.DataFrame.from_dict([res]).to_csv(f"results/transfered_models/{model_name}|{dataset_name}_all.csv", index=False)
                 pickle.dump(new_geo, open(f"results/transfered_models/{model_name}|{dataset_name}_all.pkl", "wb"))
 
